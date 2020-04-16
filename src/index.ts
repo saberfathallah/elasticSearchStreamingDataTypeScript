0067e6b0cf01ec9e66a0edaaa66be1d21b35f57b
saber <saberfathallah7@gmail.com>
2020-04-16 14:35:23 +0200

    configurate ts projet

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+dotenv.config();
+const app = express();
+
+app.use(bodyParser.json());
+
+
+app.get('/export', async (req, res) => {
+  res.json({ saber: 'saber' });
+});
+
+app.listen(4007, () => console.log('server running... '));
